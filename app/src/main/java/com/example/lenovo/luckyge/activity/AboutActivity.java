d053998d8bebac68932bf1b787afa50488c45419
LeeYujia <LeeYujia@gmail.com>
2016-08-09 13:44:01 +0800

    finish OKgit add .git add .git add .git add .!

--- app/src/main/java/com/example/lenovo/luckyge/activity/AboutActivity.java
+++ app/src/main/java/com/example/lenovo/luckyge/activity/AboutActivity.java
@@ -1,24 +1,116 @@
 package com.example.lenovo.luckyge.activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.lenovo.luckyge.R;
+import com.example.lenovo.luckyge.fragment.HomePageFragment;
 
-public class AboutActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.Map;
+
+public class AboutActivity extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
+
+    //手指向右滑动时的最小速度
+    private static final int XSPEED_MIN = 200;
+
+    //手指向右滑动时的最小距离
+    private static final int XDISTANCE_MIN = 150;
+
+    //记录手指按下时的横坐标。
+    private float xDown;
+
+    //记录手指移动时的横坐标。
+    private float xMove;
+
+    //用于计算手指滑动的速度。
+    private VelocityTracker mVelocityTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        TextView map = (TextView)findViewById(R.id.adres);
-        map.setOnClickListener(this);
+        TextView address = (TextView)findViewById(R.id.adres);
+        address.setOnClickListener(this);
+
+        LinearLayout moverlayout = (LinearLayout) findViewById(R.id.moverlayout);
+        Log.i("ll","ll=" + moverlayout);
+        moverlayout.setOnTouchListener(this);
+
+
     }
 
     @Override
     public void onClick(View v) {
+        if (v.getId()==R.id.adres){
+            Intent intent = new Intent(AboutActivity.this, MapsActivity.class);
+            startActivity(intent);
+        }
+    }
 
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        createVelocityTracker(event);
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                xDown = event.getRawX();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                xMove = event.getRawX();
+                //活动的距离
+                int distanceX = (int) (xMove - xDown);
+                //获取顺时速度
+                int xSpeed = getScrollVelocity();
+                //当滑动的距离大于我们设定的最小距离且滑动的瞬间速度大于我们设定的速度时，返回到上一个activity
+                if(distanceX > XDISTANCE_MIN && xSpeed > XSPEED_MIN) {
+                    finish();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                recycleVelocityTracker();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    /**
+     * 创建VelocityTracker对象，并将触摸content界面的滑动事件加入到VelocityTracker当中。
+     *
+     * @param event
+     *
+     */
+    private void createVelocityTracker(MotionEvent event) {
+        if (mVelocityTracker == null) {
+            mVelocityTracker = VelocityTracker.obtain();
+        }
+        mVelocityTracker.addMovement(event);
+    }
+
+    /**
+     * 回收VelocityTracker对象。
+     */
+    private void recycleVelocityTracker() {
+        mVelocityTracker.recycle();
+        mVelocityTracker = null;
+    }
+
+    /**
+     * 获取手指在content界面滑动的速度。
+     *
+     * @return 滑动速度，以每秒钟移动了多少像素值为单位。
+     */
+    private int getScrollVelocity() {
+        mVelocityTracker.computeCurrentVelocity(1000);
+        int velocity = (int) mVelocityTracker.getXVelocity();
+        return Math.abs(velocity);
     }
 }
